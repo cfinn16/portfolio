0b56d94a7699d926bafb3e22d57d4705740cddf4
cfinn16 <connorfinnegan16@gmail.com>
2019-05-23 11:05:59 -0400

    added sticky navbar functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("connected")
+
+  const navbar = document.querySelector(".navbar")
+  const sticky = navbar.offsetTop;
+
+  window.onscroll = function() {addSticky()}
+
+  const addSticky = () => {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky")
+    }
+  }
+
+})
